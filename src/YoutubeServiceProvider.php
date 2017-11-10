2a49cb27a511b53f5b8f0af26f1670f41c3f2dea
Đặng Kiên <dangtrungkien96@gmail.com>
2017-11-10 21:43:44 +0700

    first commit

---
+++ src/YoutubeServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace DangKien\Youtube;
+
+use Illuminate\Support\ServiceProvider;
+
+class YoutubeServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot()
+    {
+        $config = realpath(__DIR__.'/../config/youtube.php');
+
+        $this->publishes([$config => config_path('youtube.php')], 'config');
+
+        $this->mergeConfigFrom($config, 'youtube');
+
+        $this->publishes([
+            __DIR__.'/../migrations/' => database_path('migrations')
+        ], 'migrations');
+
+        if($this->app->config->get('youtube.routes.enabled')) {
+            include __DIR__.'/../routes/web.php';
+        }
+    }
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        $this->app->singleton('youtube', function($app) {
+            return new Youtube($app, new \Google_Client);
+        });
+    }
+}
